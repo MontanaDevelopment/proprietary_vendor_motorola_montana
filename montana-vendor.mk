e996a2990330ba74b863b91f7ef114069b5fa61e
mdeejay <mdjrussia@gmail.com>
2019-02-27 19:11:48 -0300

    montana: Add WFD blobs from daisy
    xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys

--- montana-vendor.mk
+++ montana-vendor.mk
@@ -975,8 +975,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/montana/proprietary/vendor/lib/libmmipstreamnetwork.so:system/vendor/lib/libmmipstreamnetwork.so \
     vendor/motorola/montana/proprietary/vendor/lib/libmmipstreamsourcehttp.so:system/vendor/lib/libmmipstreamsourcehttp.so \
     vendor/motorola/montana/proprietary/vendor/lib/libmmipstreamutils.so:system/vendor/lib/libmmipstreamutils.so \
-    vendor/motorola/montana/proprietary/vendor/lib/libmmosal.so:system/vendor/lib/libmmosal.so \
-    vendor/motorola/montana/proprietary/vendor/lib/libmmosal_proprietary.so:system/vendor/lib/libmmosal_proprietary.so \
     vendor/motorola/montana/proprietary/vendor/lib/libmmparser.so:system/vendor/lib/libmmparser.so \
     vendor/motorola/montana/proprietary/vendor/lib/libmmparser_lite.so:system/vendor/lib/libmmparser_lite.so \
     vendor/motorola/montana/proprietary/vendor/lib/libmmrtpdecoder.so:system/vendor/lib/libmmrtpdecoder.so \
@@ -1367,7 +1365,59 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/montana/proprietary/vendor/lib64/vendor.qti.hardware.wifi.supplicant@2.0.so:/system/vendor/lib64/vendor.qti.hardware.wifi.supplicant@2.0.so \
     vendor/motorola/montana/proprietary/vendor/lib64/vendor.qti.imsrtpservice@1.0-service-Impl.so:system/vendor/lib64/vendor.qti.imsrtpservice@1.0-service-Impl.so \
     vendor/motorola/montana/proprietary/vendor/lib64/vendor.qti.imsrtpservice@1.0_vendor.so:system/vendor/lib64/vendor.qti.imsrtpservice@1.0_vendor.so \
-    vendor/motorola/montana/proprietary/vendor/radio/qcril_database/qcril.db:system/vendor/radio/qcril_database/qcril.db
+    vendor/motorola/montana/proprietary/vendor/radio/qcril_database/qcril.db:system/vendor/radio/qcril_database/qcril.db \
+# WFD
+	vendor/motorola/montana/proprietary/bin/wfdservice:system/bin/wfdservice \
+    vendor/motorola/montana/proprietary/etc/init/wfdservice.rc:system/etc/init/wfdservice.rc \
+    vendor/motorola/montana/proprietary/etc/wfdconfig.xml:system/etc/wfdconfig.xml \
+    vendor/motorola/montana/proprietary/etc/wfdconfigsink.xml:system/etc/wfdconfigsink.xml \
+    vendor/motorola/montana/proprietary/lib/com.qualcomm.qti.wifidisplayhal@1.0.so:system/lib/com.qualcomm.qti.wifidisplayhal@1.0.so \
+    vendor/motorola/montana/proprietary/lib/extractors/libmmparser.so:system/lib/extractors/libmmparser.so \
+    vendor/motorola/montana/proprietary/lib/libaacwrapper.so:system/lib/libaacwrapper.so \
+    vendor/motorola/montana/proprietary/lib/libFileMux.so:system/lib/libFileMux.so \
+    vendor/motorola/montana/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+    vendor/motorola/montana/proprietary/lib/libmmparser_lite.so:system/lib/libmmparser_lite.so \
+    vendor/motorola/montana/proprietary/lib/libmmrtpdecoder.so:system/lib/libmmrtpdecoder.so \
+    vendor/motorola/montana/proprietary/lib/libmmrtpencoder.so:system/lib/libmmrtpencoder.so \
+    vendor/motorola/montana/proprietary/lib/libOmxMux.so:system/lib/libOmxMux.so \
+    vendor/motorola/montana/proprietary/lib/libwfdavenhancements.so:system/lib/libwfdavenhancements.so \
+    vendor/motorola/montana/proprietary/lib/libwfdclient.so:system/lib/libwfdclient.so \
+    vendor/motorola/montana/proprietary/lib/libwfdcodecv4l2.so:system/lib/libwfdcodecv4l2.so \
+    vendor/motorola/montana/proprietary/lib/libwfdcommonutils.so:system/lib/libwfdcommonutils.so \
+    vendor/motorola/montana/proprietary/lib/libwfdconfigutils.so:system/lib/libwfdconfigutils.so \
+    vendor/motorola/montana/proprietary/lib/libwfdmminterface.so:system/lib/libwfdmminterface.so \
+    vendor/motorola/montana/proprietary/lib/libwfdmmsink.so:system/lib/libwfdmmsink.so \
+    vendor/motorola/montana/proprietary/lib/libwfdmmsrc.so:system/lib/libwfdmmsrc.so \
+    vendor/motorola/montana/proprietary/lib/libwfdnative.so:system/lib/libwfdnative.so \
+    vendor/motorola/montana/proprietary/lib/libwfdrtsp.so:system/lib/libwfdrtsp.so \
+    vendor/motorola/montana/proprietary/lib/libwfdservice.so:system/lib/libwfdservice.so \
+    vendor/motorola/montana/proprietary/lib/libwfdsm.so:system/lib/libwfdsm.so \
+    vendor/motorola/montana/proprietary/lib/libwfduibcinterface.so:system/lib/libwfduibcinterface.so \
+    vendor/motorola/montana/proprietary/lib/libwfduibcsink.so:system/lib/libwfduibcsink.so \
+    vendor/motorola/montana/proprietary/lib/libwfduibcsinkinterface.so:system/lib/libwfduibcsinkinterface.so \
+    vendor/motorola/montana/proprietary/lib/libwfduibcsrc.so:system/lib/libwfduibcsrc.so \
+    vendor/motorola/montana/proprietary/lib/libwfduibcsrcinterface.so:system/lib/libwfduibcsrcinterface.so \
+    vendor/motorola/montana/proprietary/lib64/extractors/libmmparser.so:system/lib64/extractors/libmmparser.so \
+    vendor/motorola/montana/proprietary/lib64/libaacwrapper.so:system/lib64/libaacwrapper.so \
+    vendor/motorola/montana/proprietary/lib64/libFileMux.so:system/lib64/libFileMux.so \
+    vendor/motorola/montana/proprietary/lib64/libmmosal.so:system/lib64/libmmosal.so \
+    vendor/motorola/montana/proprietary/lib64/libmmparser_lite.so:system/lib64/libmmparser_lite.so \
+    vendor/motorola/montana/proprietary/lib64/libmmrtpdecoder.so:system/lib64/libmmrtpdecoder.so \
+    vendor/motorola/montana/proprietary/lib64/libmmrtpencoder.so:system/lib64/libmmrtpencoder.so \
+    vendor/motorola/montana/proprietary/lib64/libOmxMux.so:system/lib64/libOmxMux.so \
+    vendor/motorola/montana/proprietary/lib64/libwfdclient.so:system/lib64/libwfdclient.so \
+    vendor/motorola/montana/proprietary/lib64/libwfdnative.so:system/lib64/libwfdnative.so \
+    vendor/motorola/montana/proprietary/vendor/bin/wifidisplayhalservice:$(TARGET_COPY_OUT_VENDOR)/bin/wifidisplayhalservice \
+    vendor/motorola/montana/proprietary/vendor/etc/init/com.qualcomm.qti.wifidisplayhal@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/com.qualcomm.qti.wifidisplayhal@1.0-service.rc \
+    vendor/motorola/montana/proprietary/vendor/lib/com.qualcomm.qti.wifidisplayhal@1.0-halimpl.so:$(TARGET_COPY_OUT_VENDOR)/lib/com.qualcomm.qti.wifidisplayhal@1.0-halimpl.so \
+    vendor/motorola/montana/proprietary/vendor/lib/com.qualcomm.qti.wifidisplayhal@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/com.qualcomm.qti.wifidisplayhal@1.0.so \
+    vendor/motorola/montana/proprietary/vendor/lib/libmmosal_proprietary.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmosal_proprietary.so \
+    vendor/motorola/montana/proprietary/vendor/lib/libwfdcommonutils_proprietary.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwfdcommonutils_proprietary.so \
+    vendor/motorola/montana/proprietary/vendor/lib/libwfdhaldsmanager.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwfdhaldsmanager.so \
+    vendor/motorola/montana/proprietary/vendor/lib/libwfdmmservice.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwfdmmservice.so \
+    vendor/motorola/montana/proprietary/vendor/lib/libwfdmodulehdcpsession.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwfdmodulehdcpsession.so \
+    vendor/motorola/montana/proprietary/vendor/lib64/libmmosal_proprietary.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmmosal_proprietary.so \
+    vendor/motorola/montana/proprietary/vendor/lib64/libwfdhdcpcp.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwfdhdcpcp.so
 
 PRODUCT_PACKAGES += \
     CNEService \
@@ -1404,4 +1454,6 @@ PRODUCT_PACKAGES += \
 	rcsimssettings \
 	tcmclient \
     UxPerformance \
-    vendor.qti.hardware.sensorscalibrate-V1.0-java
+    vendor.qti.hardware.sensorscalibrate-V1.0-java \
+	WfdService \
+    WfdCommon
