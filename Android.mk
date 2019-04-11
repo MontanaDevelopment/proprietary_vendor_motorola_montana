30641b35afdb449ba849090032ac0ff90c2cddd7
JarlPenguin <jarlpenguin@outlook.com>
2019-04-11 18:37:09 +0300

    montana: Don't build UxPerformance
    Pre-built conflicts with PE-CAF's

--- Android.mk
+++ Android.mk
@@ -351,16 +351,6 @@ LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := UxPerformance
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/framework/UxPerformance.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := WfdService
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/WfdService/WfdService.apk
